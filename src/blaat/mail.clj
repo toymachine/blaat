c201df0a3165aa8940938008a2b2132fb29855cd
Henk Punt <henkpunt@gmail.com>
2013-12-27 16:50:42 +0100

    async mail sending

--- src/blaat/mail.clj
+++ src/blaat/mail.clj
@@ -1,16 +1,17 @@
 (ns blaat.mail
   (:require [postal.core :as p])
-  (:use [clojure.core.async :only [go]]))
+  (:use [clojure.core.async :only [thread]]))
 
 
 (def aws_smtp_user "AKIAIXQHG6G37IOWE4OQ")
 (def aws_smtp_password "Aie71HuqGN8dcaG03s3TGXR0srz80Pcv60kn/BT8OGC7")
 
+;;TODO really queue mail, or at least have an idea how deep the mail queue is
 (defn send-message [& {:keys [from to subject body] :or {from "noreply@blaat.com"
                                                          to "henkpunt@gmail.com"
                                                          subject ""
                                                          body ""}}]
-  (go
+  (thread
     (p/send-message ^{:user aws_smtp_user
                       :pass aws_smtp_password
                       :host "email-smtp.us-east-1.amazonaws.com"
@@ -20,15 +21,6 @@
 
 
 (comment
-
-  (p/send-message ^{:user aws_smtp_user
-                  :pass aws_smtp_password
-                  :host "email-smtp.us-east-1.amazonaws.com"
-                  :port 587}
-                 {:from "henkpunt@gmail.com" :to "henkpunt@gmail.com"
-                  :subject "Test from Amazon SES" :body "Test!!!2"})
-
-
 
       (send-message :subject "Please verify to enable your account"
                     :body "Please verify to enable your account")
